0a785d620f1c797e7df08be499328f4c6358904e
佛丁 <foding.xl@alibaba-inc.com>
2015-06-11 17:42:22 +0800

    commit

--- install.sh
+++ install.sh
@@ -3,15 +3,17 @@
 set -e
 
 CUR_DIR=`pwd`
-DIS_DIR='~'
+DIS_DIR='~/.vim'
 
-echo "begin to exe cp -f ${CUR_DIR} ${DIS_DIR}"
+mkdir -p "${DIS_DIR}
 
-cp -rf "${CUR_DIR}" "${DIS_DIR}"
+echo "begin to exe cp -rf ${CUR_DIR}/* ${DIS_DIR}/*"
 
-echo "begin to exe ln -f -s ${CUR_DIR}/vimrc ${DIS_DIR}/.vimrc "
+cp -rf "${CUR_DIR}/*" "${DIS_DIR}/*"
 
-ln -f -s "${CUR_DIR}/vimrc" "${DIS_DIR}/.vimrc"
+echo "begin to exe ln -f -s ${CUR_DIR}/vimrc ~/.vimrc "
+
+ln -f -s "${CUR_DIR}/vimrc" "~/.vimrc"
 
 echo "begin to exe rm -rf ${CUR_DIR}"
 
