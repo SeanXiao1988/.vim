5f145d7517e2b0ebcda601d16bafdb78bda57deb
佛丁 <foding.xl@alibaba-inc.com>
2015-06-11 20:50:53 +0800

    commit

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ mkdir -p "${DIS_DIR}"
 
 echo "begin to exe cp -rf ${CUR_DIR}/* ${DIS_DIR}"
 
-cp -rf ${CUR_DIR}/* ${DIS_DIR}
-cp -rf ${CUR_DIR}/.git ${DIS_DIR}
+cp -a -f ${CUR_DIR}/* ${DIS_DIR}
+#cp -rf ${CUR_DIR}/.git ${DIS_DIR}
 
 echo "begin to exe ln -f -s ${CUR_DIR}/vimrc ~/.vimrc "
 
@@ -22,6 +22,6 @@ rm -rf ${CUR_DIR}
 
 cd ${DIS_DIR}
 mkdir -p bundle
-git clone git@github.com:gmarik/Vundle.vim.git vundle
+git clone git@github.com:gmarik/Vundle.vim.git bundle/vundle
 
 echo "your job: vim + :BundleInstall"
