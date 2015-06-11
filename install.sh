19b6f236fa261322293ee438cac54b5b6adcd98d
佛丁 <foding.xl@alibaba-inc.com>
2015-06-11 17:34:31 +0800

    comit

--- install.sh
+++ install.sh
@@ -3,12 +3,18 @@
 set -e
 
 CUR_DIR=`pwd`
-DIS_DIR='~/'
+DIS_DIR='~'
 
 echo "begin to exe cp -f ${CUR_DIR} ${DIS_DIR}"
 
-cp -f "${CUR_DIR}" "{$DIS_DIR}"
+cp -f "${CUR_DIR}" "${DIS_DIR}"
 
-echo "begin to exe ln -f -s ${CUR_DIR}/vimrc ~/.vimrc "
+echo "begin to exe ln -f -s ${CUR_DIR}/vimrc ${DIS_DIR}/.vimrc "
 
-ln -f -s "${CUR_DIR}/vimrc"  "${DIS_DIR}/.vimrc"
+ln -f -s "${CUR_DIR}/vimrc" "${DIS_DIR}/.vimrc"
+
+echo "begin to exe rm -rf ${CUR_DIR}"
+
+rm -rf "${CUR_DIR}"
+
+echo "your job: vim + :BundleInstall"
